7903f62e3d138a1eab5594783e4a636b76e66592
Bryan DeGrendel <bryan@degrendel.com>
2014-05-31 15:19:22 -0400

    Added a version check to main
    1. Requires Love 0.9.1 now, and prints the version number on start.

--- main.lua
+++ main.lua
@@ -6,7 +6,21 @@
 require 'utilities/30logglobal'
 
 require 'utilities/core'
+require 'utilities/logging'
+
+local tag = 'Main'
+
+local function version_check()
+  if not love.getVersion then
+    Error(tag, 'love runtime is out of date! (requires 0.9.1+)')
+  else
+    local major, minor, revision = love.getVersion()
+    Log(tag, 'love runtime is (%i.%i.%i)', major, minor, revision)
+  end
+end
+
 
 function love.load()
+  version_check()
 end
 
