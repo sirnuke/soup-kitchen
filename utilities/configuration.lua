b7406837e4943751e39cdabcc1304455aeb56f73
Bryan DeGrendel <bryan@degrendel.com>
2014-05-31 18:45:08 -0400

    Updated config to match new constants format

--- utilities/configuration.lua
+++ utilities/configuration.lua
@@ -4,12 +4,20 @@
 -- See LICENSE for licensing information
 
 Configuration = Class { 
-  width = C.screen.width,
-  height = C.screen.height,
-  fullscreen = C.screen.fullscreen,
-  fullscreenType = C.screen.fullscreenType,
-  letterBoxWidth = C.screen.letterBoxWidth,
-  letterBoxHeight = C.screen.letterBoxHeight,
+  screen = {
+    size = {
+      width = C.screen.size.width,
+      height = C.screen.size.height,
+    },
+    fullscreen = {
+      enabled = C.screen.fullscreen.enabled,
+      type = C.screen.fullscreen.type,
+    },
+    letterbox = {
+      width = C.screen.letterbox.width,
+      height = C.screen.letterbox.height,
+    },
+  }
 }
 Configuration.__name = "Configuration"
 
