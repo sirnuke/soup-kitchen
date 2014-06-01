47fe8985b728ab6b15e59c8b773ab86aa4f774ca
Bryan DeGrendel <bryan@degrendel.com>
2014-06-01 15:45:21 -0400

    Squares now reference Core and draw their tile

--- map/map.lua
+++ map/map.lua
@@ -12,9 +12,17 @@ function Map:__init(core)
   for y = 1,C.map.size.width do
     local row = {}
     for x = 1,C.map.size.height do
-      table.insert(row, Square:new(C.map.layout[y][x], x, y))
+      table.insert(row, Square:new(core, C.map.layout[y][x], x, y))
     end
     table.insert(self.data, row)
+  end
+end
+
+function Map:draw()
+  for y = 1,C.map.size.width do
+    for x = 1,C.map.size.height do
+      self.data[y][x]:draw()
+    end
   end
 end
 
--- map/square.lua
+++ map/square.lua
@@ -6,15 +6,23 @@
 Square = Class()
 Square.__name = 'Square'
 
-function Square:__init(type, x, y)
+function Square:__init(core, type, x, y)
+  self.core = core
   self.quadrant = Quadrant:new(x, y)
   assert(self.quadrant:valid())
   if type == 'X' then
     self.type = 'blocked'
+    self.image = Image:new('map/blocked.png')
   elseif type == ' ' then
     self.type = 'empty'
+    self.image = Image:new('map/empty.png')
   else
     assert(false, string.format("Unknown map square type of '%s' at (%i,%i)", type, x, y))
   end
+  self.screen = self.quadrant:screen()
+end
+
+function Square:draw()
+  self.core.screen:draw(self.image.image, self.screen)
 end
 
