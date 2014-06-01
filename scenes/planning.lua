3b5d1c628545cfdf7d320ca9436bf02c0455759c
Bryan DeGrendel <bryan@degrendel.com>
2014-05-31 21:09:14 -0400

    Added click behavior and background to Planning
    1. Goes to MapView on click.
    2. Background is placeholder.

--- scenes/planning.lua
+++ scenes/planning.lua
@@ -4,23 +4,27 @@
 -- See LICENSE for licensing information
 
 Planning = Scene:extends()
-Planning.__name = "Planning"
+Planning.__name = 'Planning'
 
 function Planning:enter()
-  self.background = "placeholder"
+  self.background = Image:new('planning/background.png')
 end
 
 function Planning:exit()
+  self.background:free()
   self.background = nil
 end
 
 function Planning:draw()
-  -- TODO: Draw background
+  self.core.screen:draw(self.background.image)
 end
 
 function Planning:update(dt)
 end
 
 function Planning:mousepressed(point, button)
+  if button == 'l' then
+    self.core:switch('mapview')
+  end
 end
 
