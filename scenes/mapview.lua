df0fda9002d6f94090c441cc49f74734e8b33c1e
Bryan DeGrendel <bryan@degrendel.com>
2014-05-31 21:26:39 -0400

    Split MapView background into three pieces
    1. Two sidebars and the map background.

--- scenes/mapview.lua
+++ scenes/mapview.lua
@@ -7,16 +7,24 @@ MapView = Scene:extends()
 MapView.__name = 'MapView'
 
 function MapView:enter()
-  self.background = Image:new('mapview/background.png')
+  self.map_background = Image:new('mapview/map.png')
+  self.player_sidebar = Image:new('mapview/player-sidebar.png')
+  self.game_sidebar = Image:new('mapview/game-sidebar.png')
 end
 
 function MapView:exit()
-  self.background:free()
-  self.background = nil
+  self.map_background:free()
+  self.player_sidebar:free()
+  self.game_sidebar:free()
+  self.map_background = nil
+  self.player_sidebar = nil
+  self.game_sidebar = nil
 end
 
 function MapView:draw()
-  self.core.screen:draw(self.background.image)
+  self.core.screen:draw(self.map_background.image, C.layout.mapview.map)
+  self.core.screen:draw(self.player_sidebar.image, C.layout.mapview.player_sidebar)
+  self.core.screen:draw(self.game_sidebar.image, C.layout.mapview.game_sidebar)
 end
 
 function MapView:update(dt)
