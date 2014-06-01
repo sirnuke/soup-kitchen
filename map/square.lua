e59b725e146967a9fc1fac70c52500388dbf545d
Bryan DeGrendel <bryan@degrendel.com>
2014-06-01 17:28:20 -0400

    Converted Square to use Drawable

--- map/square.lua
+++ map/square.lua
@@ -12,17 +12,15 @@ function Square:__init(core, type, x, y)
   assert(self.quadrant:valid())
   if type == 'X' then
     self.type = 'blocked'
-    self.image = Image:new('map/blocked.png')
   elseif type == ' ' then
     self.type = 'empty'
-    self.image = Image:new('map/empty.png')
   else
     assert(false, string.format("Unknown map square type of '%s' at (%i,%i)", type, x, y))
   end
-  self.screen = self.quadrant:screen()
+  self.tile = Drawable:new(core, string.format("map/%s.png", self.type), self.quadrant:screen())
 end
 
 function Square:draw()
-  self.core.screen:draw(self.image.image, self.screen)
+  self.tile:draw()
 end
 
