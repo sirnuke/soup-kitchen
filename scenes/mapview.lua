19fed562720a04a7ef4a4f4e0ed90e1be1d3d023
Bryan DeGrendel <bryan@degrendel.com>
2014-06-01 18:11:33 -0400

    Converted MapView to use Drawable

--- scenes/mapview.lua
+++ scenes/mapview.lua
@@ -7,9 +7,12 @@ MapView = Scene:extends()
 MapView.__name = 'MapView'
 
 function MapView:enter()
-  self.map_background = Image:new('mapview/map.png')
-  self.player_sidebar = Image:new('mapview/player-sidebar.png')
-  self.game_sidebar = Image:new('mapview/game-sidebar.png')
+  self.map_background = Drawable:new(self.core, 'mapview/map.png',
+    Point:new(C.layout.mapview.map))
+  self.player_sidebar = Drawable:new(self.core, 'mapview/player-sidebar.png',
+    Point:new(C.layout.mapview.player_sidebar))
+  self.game_sidebar = Drawable:new(self.core, 'mapview/game-sidebar.png',
+    Point:new(C.layout.mapview.game_sidebar))
 end
 
 function MapView:exit()
@@ -22,9 +25,9 @@ function MapView:exit()
 end
 
 function MapView:draw()
-  self.core.screen:draw(self.map_background.image, C.layout.mapview.map)
-  self.core.screen:draw(self.player_sidebar.image, C.layout.mapview.player_sidebar)
-  self.core.screen:draw(self.game_sidebar.image, C.layout.mapview.game_sidebar)
+  self.game_sidebar:draw()
+  self.map_background:draw()
+  self.player_sidebar:draw()
   self.core.map:draw()
 end
 
