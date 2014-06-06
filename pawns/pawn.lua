0bb4372698bc900c5f2b4396cf2098a4b4860f5b
Bryan DeGrendel <bryan@degrendel.com>
2014-06-05 21:36:12 -0400

    More basic Pawn structure

--- pawns/pawn.lua
+++ pawns/pawn.lua
@@ -7,14 +7,20 @@ Pawn = Class()
 Pawn.__name = 'Pawn'
 
 function Pawn:__init()
+  self.quadrant = nil
+  self.coordinate = nil
 end
 
-function Pawn:jump(coordinate)
+function Pawn:jump(quadrant)
 end
 
-function Pawn:move(coordinate)
+function Pawn:move(quadrant)
 end
 
 function Pawn:spawned()
+end
+
+function Pawn:performDraw(img)
+  if not self:spawned() then return end
 end
 
