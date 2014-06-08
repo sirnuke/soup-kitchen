7530afc9db1fa4e054e68290f42d0e27df165b1d
Bryan DeGrendel <bryan@degrendel.com>
2014-06-08 00:30:36 -0400

    State now spawns the Coordinator

--- state/state.lua
+++ state/state.lua
@@ -9,6 +9,14 @@ State.__name = 'State'
 function State:__init(core)
   assert(core)
   self.core = core
-  self.player = Coordinator:new(core.map)
+  self.coordinator = Coordinator:new(core)
+  self.time = Time:new()
+end
+
+function State:begin()
+  self.coordinator:jump(Quadrant:new(5,5))
+end
+
+function State:update(dt)
 end
 
