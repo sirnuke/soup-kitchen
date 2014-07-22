a023cd7e2cf2ab8b3ea2bd06e83ffe4a64107cad
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 17:13:36 -0400

    Updated last two scenes to new Scene behavior

--- scenes/mapview.lua
+++ scenes/mapview.lua
@@ -7,33 +7,11 @@ MapView = Scene:extends()
 MapView.__name = 'MapView'
 
 function MapView:enter()
-  self.map_background = Drawable:new(self.core, 'mapview/map.png',
-    Point:new(C.layout.mapview.map))
-  self.player_sidebar = Drawable:new(self.core, 'mapview/player-sidebar.png',
-    Point:new(C.layout.mapview.player_sidebar))
-  self.game_sidebar = Drawable:new(self.core, 'mapview/game-sidebar.png',
-    Point:new(C.layout.mapview.game_sidebar))
-end
-
-function MapView:exit()
-  self.map_background:free()
-  self.player_sidebar:free()
-  self.game_sidebar:free()
-  self.map_background = nil
-  self.player_sidebar = nil
-  self.game_sidebar = nil
-end
-
-function MapView:draw()
-  self.game_sidebar:draw()
-  self.map_background:draw()
-  self.player_sidebar:draw()
-  self.core.map:draw()
-end
-
-function MapView:update(dt)
-end
-
-function MapView:mousepressed(point, button)
+  self:addDrawable(Drawable:new(self.core, 'mapview/map',
+    Point:new(C.layout.mapview.map)))
+  self:addDrawable(Drawable:new(self.core, 'mapview/player-sidebar',
+    Point:new(C.layout.mapview.player_sidebar)))
+  self:addDrawable(Drawable:new(self.core, 'mapview/game-sidebar',
+    Point:new(C.layout.mapview.game_sidebar)))
 end
 
--- scenes/planning.lua
+++ scenes/planning.lua
@@ -7,20 +7,8 @@ Planning = Scene:extends()
 Planning.__name = 'Planning'
 
 function Planning:enter()
-  self.background = Drawable:new(self.core, 'planning/background.png', Point:new(0, 0))
+  self:addDrawable(Drawable:new(self.core, 'planning/background', Point:new(0, 0)))
   self.core.state:begin()
-end
-
-function Planning:exit()
-  self.background:free()
-  self.background = nil
-end
-
-function Planning:draw()
-  self.background:draw()
-end
-
-function Planning:update(dt)
 end
 
 function Planning:mousepressed(point, button)
