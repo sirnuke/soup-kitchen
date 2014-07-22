e0d6f6fdb67691025f748687b4479e3e21170273
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 16:30:43 -0400

    Modified MainMenu to use default scene code

--- scenes/mainmenu.lua
+++ scenes/mainmenu.lua
@@ -7,19 +7,12 @@ MainMenu = Scene:extends()
 MainMenu.__name = 'MainMenu'
 
 function MainMenu:enter()
-  self.background = Drawable:new(self.core, 'mainmenu/background.png', Point:new(0, 0))
+  self:addDrawable(Drawable:new(self.core, 'mainmenu/background.png', Point:new(0, 0)))
 end
 
 function MainMenu:exit()
-  self.background:free()
-  self.background = nil
+  self:clearDrawables()
 end
-
-function MainMenu:draw()
-  self.background:draw()
-end
-
-function MainMenu:update(dt) end
 
 function MainMenu:mousepressed(point, button)
   if button == 'l' then self.core:switch('instructions') end
