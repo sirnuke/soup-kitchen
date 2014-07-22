b2e37ddd52034cd61e83f36657b90ffe005ad12a
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 18:00:40 -0400

    Added placeholder for map viewing prop

--- scenes/mapview.lua
+++ scenes/mapview.lua
@@ -6,12 +6,17 @@
 MapView = Scene:extends()
 MapView.__name = 'MapView'
 
+require 'scenes/mapview/map-prop'
+
 function MapView:enter()
+  local mapProp = MapProp:new()
   self:addDrawable(Drawable:new(self.core, 'mapview/map',
     Point:new(C.layout.mapview.map)))
   self:addDrawable(Drawable:new(self.core, 'mapview/player-sidebar',
     Point:new(C.layout.mapview.player_sidebar)))
   self:addDrawable(Drawable:new(self.core, 'mapview/game-sidebar',
     Point:new(C.layout.mapview.game_sidebar)))
+  self:addDrawable(mapProp)
+  --self.addInteractable(mapProp)
 end
 
---
+++ scenes/mapview/map-prop.lua
@@ -0,0 +1,14 @@
+-- Soup Kitchen
+-- Bryan DeGrendel (c) 2014
+--
+-- See LICENSE for licensing information
+
+MapProp = Class()
+MapProp.__name = 'MapProp'
+
+function MapProp:__init()
+end
+
+function MapProp:draw()
+end
+
