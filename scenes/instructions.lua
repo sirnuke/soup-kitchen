54b864c3ff1e909e1fd3bae1599d8d4c075ca364
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 17:06:32 -0400

    Updated Instructions to the new scenes behavior

--- scenes/instructions.lua
+++ scenes/instructions.lua
@@ -7,19 +7,8 @@ Instructions = Scene:extends()
 Instructions.__name = 'Instructions'
 
 function Instructions:enter()
-  self.background = Drawable:new(self.core, 'instructions/background.png', Point:new(0, 0))
+  self:addDrawable(Drawable:new(self.core, 'instructions/background', Point:new(0, 0)))
 end
-
-function Instructions:exit()
-  self.background:free()
-  self.background = nil
-end
-
-function Instructions:draw()
-  self.background:draw()
-end
-
-function Instructions:update(dt) end
 
 function Instructions:mousepressed(point, button)
   if button == 'l' then self.core:switch('planning') end
