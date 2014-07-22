afeb6cf96f7b544200e898a9cb0ca16aed3bce3d
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 16:53:42 -0400

    Updated Drawable to match new image behavior

--- ui/drawable.lua
+++ ui/drawable.lua
@@ -8,19 +8,20 @@ Drawable.__name = "Drawable"
 
 -- Note that point is not duplicated.  The idea that the caller has setup
 -- a dedicated point already when creating the Drawable.
-function Drawable:__init(core, filename, point)
-  assert(core and type(filename) == 'string' and point)
-  self.image = Image:new(filename)
+function Drawable:__init(core, image, point)
+  assert(core and image and point)
+  if type(image) == 'string' then
+    self.image = Image:new(image)
+  elseif type(image) == 'table' and image:is(Image) then
+    self.image = image
+  else
+    assert(false, string.format("Unknown Drawable image parameter %s", image))
+  end
   self.core = core
   self.point = point
 end
 
-function Drawable:free()
-  self.image:free()
-  self.image = nil
-end
-
 function Drawable:draw()
-  self.core.screen:draw(self.image.image, self.point)
+  self.core.screen:draw(self.image.data, self.point)
 end
 
