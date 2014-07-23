f35c8d8f75159aed910b216810ee28714178991e
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 21:48:59 -0400

    Updated InGame to MapView and constants

--- scenes/ingame.lua
+++ scenes/ingame.lua
@@ -7,14 +7,12 @@ InGame = Scene:extends()
 InGame.__name = 'InGame'
 
 function InGame:enter()
-  local mapView = MapView:new()
-  self:addDrawable(Drawable:new(self.core, 'ingame/map',
-    Point:new(C.layout.mapview.map)))
+  local mapView = MapView:new(self)
   self:addDrawable(Drawable:new(self.core, 'ingame/player-sidebar',
-    Point:new(C.layout.mapview.player_sidebar)))
+    Point:new(C.layout.ingame.playersidebar.point)))
   self:addDrawable(Drawable:new(self.core, 'ingame/game-sidebar',
-    Point:new(C.layout.mapview.game_sidebar)))
-  self:addDrawable(mapProp)
+    Point:new(C.layout.ingame.gamesidebar.point)))
+  self:addDrawable(mapView)
   --self.addInteractable(mapProp)
 end
 
