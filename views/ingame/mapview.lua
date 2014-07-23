ff860165d9eaa50e3c750065f404c4aa4ff38524
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 21:48:50 -0400

    Updated MapView to draw the map
    1. Currently just a placeholder background.

--- views/ingame/mapview.lua
+++ views/ingame/mapview.lua
@@ -6,9 +6,14 @@
 MapView = Class()
 MapView.__name = 'MapView'
 
-function MapView:__init()
+function MapView:__init(scene)
+  self.scene = scene
+  self.point = C.layout.ingame.mapview.point
+  self.size = C.layout.ingame.mapview.size
+  self.background = Drawable:new(scene.core, 'ingame/map', self.point)
 end
 
 function MapView:draw()
+  self.background:draw()
 end
 
