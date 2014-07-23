0726dcf9e5c9d5eed58e38aee313ddc8131a4e6b
Bryan DeGrendel <bryan@degrendel.com>
2014-07-22 22:38:39 -0400

    MapView now draws all squares

--- scenes/ingame.lua
+++ scenes/ingame.lua
@@ -7,7 +7,7 @@ InGame = Scene:extends()
 InGame.__name = 'InGame'
 
 function InGame:enter()
-  local mapView = MapView:new(self)
+  local mapView = MapView:new(self.core, self.core.map)
   self:addDrawable(Drawable:new(self.core, 'ingame/player-sidebar',
     Point:new(C.layout.ingame.playersidebar.point)))
   self:addDrawable(Drawable:new(self.core, 'ingame/game-sidebar',
--- views/ingame/mapview.lua
+++ views/ingame/mapview.lua
@@ -6,14 +6,29 @@
 MapView = Class()
 MapView.__name = 'MapView'
 
-function MapView:__init(scene)
-  self.scene = scene
+function MapView:__init(core, map)
+  self.core = core
+  self.map = map
   self.point = C.layout.ingame.mapview.point
   self.size = C.layout.ingame.mapview.size
-  self.background = Drawable:new(scene.core, 'ingame/map', self.point)
+  self.background = Drawable:new(core, 'ingame/map', self.point)
+  self.squares = {}
+
+  local quadrant = Quadrant:new()
+  for y,row in pairs(map.data) do
+    quadrant.y = y
+    for x,square in pairs(row) do
+      quadrant.x = x
+      table.insert(self.squares, Drawable:new(self.core, string.format('map/%s', square.type),
+        quadrant:screen()))
+    end
+  end
 end
 
 function MapView:draw()
   self.background:draw()
+  for k,square in pairs(self.squares) do
+    square:draw()
+  end
 end
 
