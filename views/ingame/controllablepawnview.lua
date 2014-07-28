61f23f92c3f6bb41dbae38fbda79f94f928e1e08
Bryan DeGrendel <bryan@degrendel.com>
2014-07-27 23:29:06 -0400

    Pawn selected and movement logic structure
    1. Doesn't actually move yet, because that part of pawn isn't completed.

--- views/ingame/controllablepawnview.lua
+++ views/ingame/controllablepawnview.lua
@@ -9,5 +9,24 @@ ControllablePawnView.__name = "ControllablePawnView"
 function ControllablePawnView:__init(core, pawn)
   assert(core, pawn)
   ControllablePawnView.super.__init(self, core, pawn)
+  self.selected = false
+end
+
+function ControllablePawnView:mouseSelectedPressed(point)
+  local screen = self.pawn.coordinate:screen()
+  if point.x >= screen.x and point.x < screen.x + C.map.pawn.width
+    and point.y >= screen.y and point.y < screen.y + C.map.pawn.height then
+    if not self.selected then Log(self, "Selected!") end
+    self.selected = true
+  else
+    if self.selected then Log(self, "Unselected!") end
+    self.selected = false
+  end
+end
+
+function ControllablePawnView:mouseMovementPressed(quadrant)
+  if self.selected then
+    -- TODO: Attempt to send pawn to this quadrand
+  end
 end
 
