2fa7fdd36a32b59a8a571283eefdc0a67a3a97ac
Bryan DeGrendel <bryan@degrendel.com>
2014-08-06 17:20:31 -0400

    Steps towards supporting moving pawns

--- pawns/pawn.lua
+++ pawns/pawn.lua
@@ -12,6 +12,7 @@ function Pawn:__init(map, id, controllable)
   self.map = map
   self.quadrant = nil
   self.coordinate = nil
+  self.destination = nil
   self.spawned = false
   self.controllable = controllable
 end
@@ -22,9 +23,14 @@ function Pawn:jump(quadrant)
   self.coordinate = quadrant:coordinate(C.map.pawn.width / 2, C.map.pawn.height / 2)
   Log(self, 'pawn coordinate is %s, compare to %s', self.coordinate, quadrant:coordinate())
   self.spawned = true
-  self.map:jump(self, quadrant)
+  self.map:move(self, quadrant)
 end
 
 function Pawn:move(quadrant)
+  assert(quadrant)
+  assert(self.quadrant and self.spawned, string.format("Cannot move %s, not spawned", self))
+  self.destination = quadrant
+  self.map:move(self, quadrant)
+  local start = self.coordinate:quadrant()
 end
 
